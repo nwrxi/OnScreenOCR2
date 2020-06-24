fad399cf2b354bb5248ff4377190826fca5439c8
Aleks <aleksf11125@outlook.com>
2020-06-24 11:43:30 +0200

    Added ability to pass Language Hints to API.

--- OnScreenOCR/PerformOCR/VisionApi.cs
+++ OnScreenOCR/PerformOCR/VisionApi.cs
@@ -16,6 +16,15 @@ namespace OnScreenOCR.PerformOCR
             var image = await Task.Run(() => Image.FromBytes(imageArr));
             var client = await ImageAnnotatorClient.CreateAsync();
 
+
+            //var imageContext = new ImageContext();
+            //imageContext.LanguageHints.Add("ja");
+            //var request = new AnnotateImageRequest { ImageContext = imageContext, Image = image, Features = { new Feature { Type = Feature.Types.Type.TextDetection } } };
+            //var response = await client.AnnotateAsync(request);
+
+            //return response.TextAnnotations.ToString() == "[ ]" ? "No text found" : response.TextAnnotations[0].Description; ;
+
+
             var textAnnotations = await client.DetectTextAsync(image);
             return textAnnotations.ToString() == "[ ]" ? "No text found" : textAnnotations[0].Description;
         }
